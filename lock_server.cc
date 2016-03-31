b0886bcb5b7ea65dd69ccd9b9d9cc299936a8232
phlalx <phlalx@users.noreply.github.com>
2016-03-31 11:19:51 +0000

    Added acquire and release RPCs, they don't do anything yet

--- lock_server.cc
+++ lock_server.cc
@@ -20,4 +20,20 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  printf("acquire request from clt %d\n", clt);
+  r = 0;
+  return ret;
+}
 
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  printf("release request from clt %d\n", clt);
+  r = 0;
+  return ret;
+}

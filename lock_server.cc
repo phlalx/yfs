283f76ec8d0cd8b428982fe85bf8b29caf20bf67
phlalx <phlalx@users.noreply.github.com>
2016-04-12 14:51:44 +0000

    acquire/release done (using condition variables)

--- lock_server.cc
+++ lock_server.cc
@@ -23,17 +23,43 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
 lock_protocol::status
 lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
 {
+  pthread_mutex_lock(&mutex);
   lock_protocol::status ret = lock_protocol::OK;
   printf("acquire request from clt %d\n", clt);
-  r = 0;
+
+  if (locks.find(lid) == locks.end()) {
+   	locks[lid] = lock_info();
+  } 
+
+  lock_info &li = locks[lid];
+
+  if (li.is_locked) {
+    pthread_cond_wait(&li.waiting, &mutex);
+    assert(!li.is_locked);
+  }
+
+  li.is_locked = true;
+  pthread_mutex_unlock(&mutex);
   return ret;
 }
 
 lock_protocol::status
 lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
 {
+  pthread_mutex_lock(&mutex);
   lock_protocol::status ret = lock_protocol::OK;
   printf("release request from clt %d\n", clt);
-  r = 0;
+
+  if (locks.find(lid) == locks.end()) {
+  	assert(false);
+  } else {
+    lock_info &li = locks[lid];
+    assert(li.is_locked);
+    li.is_locked = false; 
+    printf("unlock %d\n", lid);
+    pthread_cond_signal(&li.waiting);
+  }
+  pthread_mutex_unlock(&mutex);
+
   return ret;
 }

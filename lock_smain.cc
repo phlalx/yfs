1b0586c4645ebf1cc080f77acc5e410712d60715
phlalx <phlalx@users.noreply.github.com>
2016-05-26 16:34:45 +0000

    lab4 -> switch to cache lock server, doesn't work yet

--- lock_smain.cc
+++ lock_smain.cc
@@ -2,7 +2,6 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
-//#include "lock_server.h"
 #include <sys/types.h>
 #include <unistd.h>
 #include "lock_server_cache.h"
@@ -34,11 +33,11 @@ main(int argc, char *argv[])
   jsl_set_debug(JSL_DBG_ME);
 
 #ifndef RSM
-  lock_server ls;
+  lock_server_cache ls;
   rpcs server(atoi(argv[1]), count);
-  server.reg(lock_protocol::stat, &ls, &lock_server::stat);
-  server.reg(lock_protocol::acquire, &ls, &lock_server::acquire);
-  server.reg(lock_protocol::release, &ls, &lock_server::release);
+  server.reg(lock_protocol::stat, &ls, &lock_server_cache::stat);
+  server.reg(lock_protocol::acquire, &ls, &lock_server_cache::acquire);
+  server.reg(lock_protocol::release, &ls, &lock_server_cache::release);
 #endif
 
 
